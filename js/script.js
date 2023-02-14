359cc4068d56dcd0197d3a1e63578e98fb89ed9c
Ian Stewart <ianstewart4@outlook.com>
2023-02-13 19:00:44 -0600

    adding animations and updated styling

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
 });
+
+const faders = document.querySelectorAll('.fade-in')
+const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -10px 0px"
+}
+
+const appearOnScroll = new IntersectionObserver(
+    function (entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return
+            } else {
+                entry.target.classList.add('appear')
+                appearOnScroll.unobserve(entry.target)
+            }
+        })
+    }, appearOptions)
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader)
+})
